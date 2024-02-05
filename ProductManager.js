559b081edf34726d0f83bd77ccca87ee9ca6a652
Ale-Campos <alejandrocampos9092@gmail.com>
2024-02-05 17:00:49 -0300

    Primer Commit

---
+++ ProductManager.js
@@ -0,0 +1,88 @@
+class Product {
+    constructor(title, description, price, thumbnail, code, stock){
+        this.title = title
+        this.description = description
+        this.price = price
+        this.thumbnail = thumbnail
+        this.code = code //Identificador
+        this.stock = stock
+    }
+}
+
+class ProductManager {
+
+    static totalProductCounter = 0
+
+    constructor(){
+        this.products = []
+    }
+    
+    isValid(product) {
+        return (
+            product.title != undefined &&
+            product.description != undefined &&
+            product.price != undefined &&
+            product.thumbnail != undefined &&
+            product.code != undefined &&
+            product.stock != undefined
+        )
+    }
+
+    getProducts() {
+        return this.products
+    }
+
+    addProduct(product){
+
+        if(this.isValid(product)) {
+
+            let existingPorduct = this.products.find(prod => prod.code == product.code)
+            
+            if(existingPorduct) {
+                console.log("El producto ya existe")
+            } else {
+                product.id = this.products.length + 1
+                this.products.push(product)
+                ProductManager.totalProductCounter++
+            }
+        } else {
+            console.log("El producto tiene campos incompletos");
+        }
+        
+        return this.products
+    }
+
+    removeProduct(code) {
+
+        if(code) {
+
+            let product = this.products.find(prod => prod.code == code)
+    
+            if(product){
+                this.products = this.products.filter(prod => prod.code != code)
+            }
+        }
+        
+        return this.products
+    }
+}
+
+let pm = new ProductManager
+
+let product = new Product('Producto1', 'Descripcion 1', 100, 'url', '122333', 100)
+let product2 = new Product('Producto1', 'Descripcion 1', 100, 'url', '455678', 100)
+let product3 = new Product('Producto1', 'Descripcion 1', 100, 'url', '787844', 100)
+let product4 = new Product('Producto1', 'Descripcion 1', 100, 'url', '993664', 100)
+let duplicatedProduct = new Product('Producto1', 'Descripcion 1', 100, 'url', '122333', 100)
+let invalidProduct = new Product('Producto1', 100, 'url', '122333', 100) // Sin decripcion
+
+pm.addProduct(product)
+pm.addProduct(product2)
+pm.addProduct(product3)
+pm.removeProduct(product3.code)
+pm.addProduct(product4)
+pm.addProduct(invalidProduct)
+pm.addProduct(duplicatedProduct)
+
+console.log(pm.getProducts())
+console.log(ProductManager.totalProductCounter); // Marca 3 porque producto 3 fue agregado y luego eliminado
